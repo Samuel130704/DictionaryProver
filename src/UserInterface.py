2437e3510be39593503fbf84796b9fc0e50cf5f4
Samuel130704 <kleinlerchersamuel882@gmail.com>
2023-12-09 12:59:52 +0100

    v1.1

--- src/UserInterface.py
+++ src/UserInterface.py
@@ -8,41 +8,36 @@ def read_inputs():
     root.geometry("1000x500")
     letters = list()
 
-    def save_input():
-        inp = input1.get()
+    def save_letters():
+        inp = let_inp.get()
 
         if len(inp) < 2 and inp.isalpha():
             letters.append(inp)
-            label2.configure(text=inp)
-            input1.delete(0, 'end')
+            let_lbl2.configure(text=inp)
+            let_inp.delete(0, 'end')
         else:
-            label2.configure(text="Invalid Input")
-            input1.delete(0, 'end')
+            let_lbl2.configure(text="Invalid Input")
+            let_inp.delete(0, 'end')
 
         new_letters = dic.throw_out_similar(letters)
         print(new_letters)
 
-    def next_input():
-        frame1.pack_forget()
-        frame2.pack()
+    let_frm = ttk.Frame()
+    req_let_frm = ttk.Frame()
+    occ_frm = ttk.Frame()
+    max_let_frm = ttk.Frame()
+    min_let_frm = ttk.Frame()
+    fin_btn_frm = ttk.Frame()
 
-    frame1 = ttk.Frame()
-    frame2 = ttk.Frame()
+    let_lbl1 = ttk.Label(master=let_frm, text="Type the letters you want in your words")
+    let_lbl1.pack()
+    let_inp = ttk.Entry(master=let_frm)
+    let_inp.pack()
+    let_btn = ttk.Button(master=let_frm, text="Press to confirm the letters", command=save_letters)
+    let_btn.pack()
+    let_lbl2 = ttk.Label(master=let_frm)
+    let_lbl2.pack()
 
-    label1 = ttk.Label(master=frame1, text="Type in a letter")
-    label1.pack()
-    label2 = ttk.Label(master=frame1)
-    label2.pack()
-
-    input1 = ttk.Entry(master=frame1)
-    input1.pack()
-
-    button1 = ttk.Button(master=frame1, text="Press to insert the letter", command=save_input)
-    button1.pack()
-    button2 = ttk.Button(master=frame2, text="Press to end the input", command=next_input)
-    button2.pack()
-
-    frame1.pack()
-    frame2.pack()
+    let_frm.pack()
 
     root.mainloop()
